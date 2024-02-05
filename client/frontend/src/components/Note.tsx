93e0165a4acf0423e256d35eeee666d5a56735d0
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-02-05 08:02:05 +0100

    Refactor Note and Home components UI

--- client/frontend/src/components/Note.tsx
+++ client/frontend/src/components/Note.tsx
@@ -1,28 +1,43 @@
-
 export type NoteProps = {
-    category: string;
-    title: string;
-    description: string;
-    date: string;
-    completed: boolean;
-}
+  category: string;
+  title: string;
+  description: string;
+  date: string;
+  completed: boolean;
+};
 
-const Note = ({category, title, description, date, completed}: NoteProps) => {
+const Note = ({ category, title, description, date, completed }: NoteProps) => {
   return (
     <section className="custom-card">
       <div className="flex justify-between">
-          <h3 className="category-badge ${completed ? 'color' : null}">${category}</h3>
-          <div className="flex items-center">
-              <input type="checkbox" checked={completed} name="" id="" className="checkbox-input" />
-              <button className="edit-button ${completed ? 'color' : null}"><img src="./assets/pencil.svg" alt="" /></button>
-              <button className="delete-bin ${completed ? 'color' : null}"><img src="./assets/bin.svg" alt="" /></button>
-          </div>
+        <h3 className="category-badge ${completed ? 'color' : null}">
+          {category}
+        </h3>
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            checked={completed}
+            name=""
+            id=""
+            className="checkbox-input"
+          />
+          <button className="edit-button ${completed ? 'color' : null}">
+            <img src="./assets/pencil.svg" alt="" />
+          </button>
+          <button className="delete-bin ${completed ? 'color' : null}">
+            <img src="./assets/bin.svg" alt="" />
+          </button>
+        </div>
       </div>
-      <p className={`task-title ${completed ? 'line-through' : null}`}>{title}</p>
-      <p className={`task-description ${completed ? 'line-through' : null}`}>{description}</p>
-      <p className={`task-date ${completed ? 'line-through' : null}`}>{date}</p>
+      <p className={`task-title ${completed ? "line-through" : null}`}>
+        {title}
+      </p>
+      <p className={`task-description ${completed ? "line-through" : null}`}>
+        {description}
+      </p>
+      <p className={`task-date ${completed ? "line-through" : null}`}>{date}</p>
     </section>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
--- client/frontend/src/pages/Home.tsx
+++ client/frontend/src/pages/Home.tsx
@@ -1,45 +1,50 @@
-import { useEffect, useState } from "react"
-import Note from "../components/Note"
+import { useEffect, useState } from "react";
+import Note from "../components/Note";
 
 type Notes = {
-    id: number;
-    category: string;
-    title: string;
-    description: string;
-    date: string;
-    completed: boolean;
-}[]
+  id: number;
+  category: string;
+  title: string;
+  description: string;
+  date: string;
+  completed: boolean;
+}[];
 
 const Home = () => {
-    // const apiUrl = 'https://notely-orcin.vercel.app/notes'
-    const [notes, setNotes] = useState<Notes>([])
-    const [isLoading, setIsLoading] = useState(true)
-    // const [error, setError] = useState(null)
+  // const apiUrl = 'https://notely-orcin.vercel.app/notes'
+  const [notes, setNotes] = useState<Notes>([]);
+  //   const [isLoading, setIsLoading] = useState(true);
+  //   // const [error, setError] = useState(null)
 
-    useEffect(() => {
-        const fetchNotes = async () => {
-            try {
-                const res = await fetch('https://notely-orcin.vercel.app/notes')
-                const data = await res.json()
-                console.log(data)
-                setNotes(data)
-                setIsLoading(false)
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        fetchNotes()
-    }, [])
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await fetch("https://notely-orcin.vercel.app/notes");
+        const data = await res.json();
+        console.log(data);
+        setNotes(data);
+        // setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchNotes();
+  }, []);
 
   return (
-    <div>
-        {
-            notes.map(note => (
-                <Note key={note.id} category={note.category} title={note.title} description={note.description} date={note.date} completed={note.completed} />
-            ))
-        }
-    </div>
-  )
-}
+    <>
+      {notes.map((note) => (
+        <Note
+          key={note.id}
+          category={note.category}
+          title={note.title}
+          description={note.description}
+          date={note.date}
+          completed={note.completed}
+        />
+      ))}
+    </>
+  );
+};
 
-export default Home
+export default Home;

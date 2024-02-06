2d610f8a74a9a7d13dfaae546e25af737dde73a0
Gandort <oluwasemiloreakinnuli@gmail.com>
2024-02-06 23:33:04 +0100

    Refactor AddModal component

--- client/frontend/src/components/AddModal.tsx
+++ client/frontend/src/components/AddModal.tsx
@@ -2,13 +2,45 @@
 const AddModal = ({setIsOpened}:  {setIsOpened: React.Dispatch<React.SetStateAction<boolean>>}) => {
     return (
       <main className="w-full h-full backdrop top-0 left-0">
-        <section className="delete-modal">
-          <p className="heading">Delete Note</p>
-          <p className="">Are you sure you want to delete this note?</p>
-          <div className="button-container">
-            <button className="cancel-button" onClick={()=> setIsOpened(false)}>Cancel</button>
-            <button className="delete-button">Delete</button>
-          </div>
+        <section className="modal">
+          <p className="heading">Add Note</p>
+          <form action="" className="modal-form">
+            <div className="input-container">
+              <label htmlFor="title">Title</label>
+              <input
+               type="text" 
+               id="title" 
+               name="title" 
+               className="title-input"
+              />
+            </div>
+            <div className="input-container">
+              <label htmlFor="category">Category</label>
+              <select name="category" id="category" className="category-input">
+                <option value="work">Work</option>
+                <option value="personal">Personal</option>
+                <option value="others">Others</option>
+              </select>
+            </div>
+            <div className="input-cotainer description-container">
+              <label htmlFor="content">Description(optional)</label>
+              <textarea 
+                name="content" 
+                id="content" 
+                maxLength={180} 
+                cols={30} 
+                rows={10}
+                placeholder="Add a description... (Max 180 characters)"
+                className="description-input" 
+              >
+
+              </textarea>
+            </div>
+            <div className="button-container">
+              <button type="submit" className="cancel-button" onClick={() => setIsOpened(false)}>Cancel</button>
+              <button type="button" className="add-button">Cancel</button>
+            </div>
+          </form>
         </section>
       </main>
     )

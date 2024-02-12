04669a16c212a051683574d934fd8efc1bb89973
AkinnuliFavour <123216973+AkinnuliFavour@users.noreply.github.com>
2024-02-12 12:51:21 +0000

    Update allowedOrigins.js to include GitHub.dev URL

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
    'http://localhost:5173',
    'https://notely-frontend.vercel.app',
-   'https://crispy-space-fiesta-69v94x664x434rrr-5173.app.github.dev/'
+   'https://crispy-space-fiesta-69v94x664x434rrr-5173.app.github.dev'
 ]
 
 module.exports = allowedOrigins

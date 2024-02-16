0e380c3e9dc0d4f21b59ea1080541093d0285301
AkinnuliFavour <123216973+AkinnuliFavour@users.noreply.github.com>
2024-02-16 15:31:51 +0000

    Add new allowed origin for Notely React frontend

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
    'http://localhost:5173',
    'https://notely-frontend.vercel.app',
-   'https://crispy-space-fiesta-69v94x664x434rrr-5173.app.github.dev'
+   'https://crispy-space-fiesta-69v94x664x434rrr-5173.app.github.dev',
+   'https://notely-react-frontend.vercel.app'
 ]
 
 module.exports = allowedOrigins

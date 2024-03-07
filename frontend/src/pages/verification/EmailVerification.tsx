d88853af7d835557ba5085006b404324bef28c56
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-03-07 13:23:10 +0100

    Update email verification heading alignment

--- frontend/src/pages/verification/EmailVerification.tsx
+++ frontend/src/pages/verification/EmailVerification.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const EmailVerification = () => {
     return (
         <main className="bg-blue-500 h-screen flex flex-col justify-center items-center text-white">
-            <h1 className="text-3xl font-bold">Congratulations! Your Email has been successfully verified</h1>
+            <h1 className="text-3xl font-bold text-center">Congratulations! Your Email has been successfully verified</h1>
             <Link to='/sign-in'><button className="mt-3 bg-blue-400 px-8 py-3 rounded-lg">Sign In</button></Link>
         </main>
     )
